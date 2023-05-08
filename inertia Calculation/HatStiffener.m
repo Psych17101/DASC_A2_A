417d3d462a0233d0841afe9db41de83c9e76fe32
christophe <christophe@hatterer.net>
2023-05-08 11:59:44 +0200

    Images
    Images

--- inertia Calculation/HatStiffener.m
+++ inertia Calculation/HatStiffener.m
@@ -31,7 +31,7 @@ xy13 = [-b/2 0];
 
 xy = [xy1;xy2;xy3;xy4;xy5;xy6;xy7;xy8;xy9;xy10;xy11;xy12;xy13];
 
-Polygon=PolygonMoments(xy,[],-4)
+Polygon=PolygonMoments(xy,[],-3)
 disp 'In this example, because of the 360 degree symmetry,'
 disp 'any x-y axes are principal axes'
 
